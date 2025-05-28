11c2de91daddccb140bb6b6e0605d7b11e2b77b5
brage-grrgrr <brage.w.b@gmail.com>
2025-05-28 08:15:53 +0000

    lagde de basice tingen til å ha html

---
+++ Registrer.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Registrering</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+</body>
+</html>
